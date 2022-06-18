0a2b5204d6e51b08f7ec5f27551d71fd07f7e483
Menghetz <matteo.menghetti@gmail.com>
2022-06-18 22:18:09 +0100

    added publish to pubsub

--- citations_scrape/citations.py
+++ citations_scrape/citations.py
@@ -4,6 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 import unicodedata
 from headers import *
+import base64
+import os
+
+from google.cloud import pubsub_v1
 
 
 def scrape_citations():
@@ -29,4 +33,36 @@ def scrape_citations():
         quote['author'] = row.img['alt'].split(" #")[1]
         quotes.append(quote)
     #print(quotes)
+    publish_to_pubsub(json.dumps(quotes))
     return json.dumps(quotes)
+
+
+def publish_to_pubsub(message):
+
+    # Instantiates a Pub/Sub client
+    publisher = pubsub_v1.PublisherClient()
+    PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
+
+    topic_name = os.environ.get("TOPIC")
+
+    if not topic_name or not message:
+        return ('Missing "topic" and/or "message" parameter.', 400)
+
+    print(f'Publishing message to topic {topic_name}')
+
+    # References an existing topic
+    topic_path = publisher.topic_path(PROJECT_ID, topic_name)
+
+    message_json = json.dumps({
+        'data': {'message': message},
+    })
+    message_bytes = message_json.encode('utf-8')
+
+    # Publishes a message
+    try:
+        publish_future = publisher.publish(topic_path, data=message_bytes)
+        publish_future.result()  # Verify the publish succeeded
+        return 'Message published.'
+    except Exception as e:
+        print(e)
+        return (e, 500)
